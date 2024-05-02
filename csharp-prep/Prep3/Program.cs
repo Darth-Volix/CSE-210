4ef7be334990cddd34ede9e3b3b63569efd84c30
Darth-Volix <wil22159@byui.edu>
2024-05-02 09:33:37 -0600

    started prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+       int number = randomGenerator.Next(1, 11);
+
+       Console.WriteLine("Welcome to the Magic Number Guessing Game!");
+
+       string user_input;
+       int user_guess;
+
+       Console.Write("What is your guess? ");
+       user_input = Console.ReadLine();
+       user_guess= int.Parse(user_input);
+
+       if (user_guess < number)
+       {
+        Console.WriteLine("Higher");
+       }
+       else if (user_guess > number)
+       {
+        Console.WriteLine("Lower");
+       }
+       else
+       {
+        Console.WriteLine("You guessed it!");
+       }
+
     }
 }
