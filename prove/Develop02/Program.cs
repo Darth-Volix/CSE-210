3f8899211397474b56b7ae9de1e1fdfcf31c936d
Darth-Volix <wil22159@byui.edu>
2024-05-16 22:24:55 -0600

    chore: Refactor Journal and Program classes to add methods for saving and loading journal entries from a text file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,64 +3,6 @@ using System.IO;
 
 class Program
 {
-    // Method to save journal to a text file 
-    public static void SaveToFile(Journal journal)
-    {
-        try
-        {
-            // Prompt the user for a file name
-            Console.WriteLine("Enter the name of the file to save to: ");
-            string fileName = Console.ReadLine();
-
-            // Write the journal entries to the file
-            using (StreamWriter sw = File.CreateText(fileName))
-            {
-                foreach (JournalEntry entry in journal._journalEntries)
-                {
-                    sw.WriteLine($"{entry._entryDateTime}\\{entry._entryPrompt}\\{entry._entry}");
-                }
-            }
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("File not found.");
-        }
-    }
-
-    // Method to load journal from a text file
-    public static Journal LoadFromFile()
-    {
-        try
-        {
-            // Create a new journal object
-            Journal journal = new Journal();
-
-            // Prompt the user for a file name
-            Console.WriteLine("Enter the name of the file to load: ");
-            string fileName = Console.ReadLine();
-
-            // Read the file into an array of strings
-            string[] lines = System.IO.File.ReadAllLines(fileName);
-
-            // Create a new journal entry for each entry in the file
-            JournalEntry entry = new JournalEntry();
-            foreach (string line in lines)
-            {
-                string[] parts = line.Split('\\');
-                entry._entryDateTime = parts[0];
-                entry._entryPrompt = parts[1];
-                entry._entry = parts[2];
-                journal.StoreJournalEntry(entry);
-            }
-            return journal;
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("File not found.");
-            return null;
-        }
-    }
-
     public static void Menu()
     {
         Console.WriteLine("");
@@ -99,11 +41,11 @@ class Program
             }
             else if (choice == "3")
             {
-                SaveToFile(activeJournal);
+                Journal.SaveToFile(activeJournal);
             }
             else if (choice == "4")
             {
-                activeJournal = LoadFromFile();
+                activeJournal = Journal.LoadFromFile();
             }
             else if (choice == "5")
             {
