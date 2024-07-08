4089fd7d082fcd933de0650443654066de9a9974
Darth-Volix <wil22159@byui.edu>
2024-07-08 14:34:28 -0600

    chore: Refactor MakeDeposit method in DepositAccount class and add MakeWithdrawal, CalculateMonthlyInterestRate, and AddInterest methods

--- final/FinalProject/DepositAccount.cs
+++ final/FinalProject/DepositAccount.cs
@@ -20,10 +20,39 @@ public class DepositAccount
         _closeDate = DateTime.Now;
     }
 
-    public virtual void MakeDeposit(decimal depositAmount)
+    public virtual void MakeDeposit()
     {
+        Console.Write("\nEnter the amount you would like to deposit: ");
+        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+
         _balance += depositAmount;
         _transactions.Add(new Transaction(depositAmount, "Deposit", DateTime.Now));
         Console.WriteLine($"\nDeposit of {depositAmount} made on {DateTime.Now}");
+    }
+
+    public virtual void MakeWithdrawal()
+    {
+        Console.Write("\nEnter the amount you would like to withdraw: ");
+        decimal withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+
+        _balance -= withdrawalAmount;
+        _transactions.Add(new Transaction(withdrawalAmount, "Withdrawal", DateTime.Now));
+        Console.WriteLine($"\nWithdrawal of {withdrawalAmount} made on {DateTime.Now}");
+    }
+
+    public static decimal CalculateMonthlyInterestRate()
+    {
+        decimal monthlyInterestRate = _interestRate / 12;
+
+        return monthlyInterestRate;
+    }
+
+    public static void AddInterest()
+    {
+        decimal monthlyInterestRate = CalculateMonthlyInterestRate();
+        decimal monthlyInterest = _balance * monthlyInterestRate;
+        _balance += monthlyInterest;
+        _transactions.Add(new Transaction(monthlyInterest, "Interest", DateTime.Now));
+        Console.WriteLine($"\nInterest of {monthlyInterest} added on {DateTime.Now}");
     }
 }
